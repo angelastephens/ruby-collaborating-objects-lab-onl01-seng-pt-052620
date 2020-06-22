053aeb8d5fb021fa989ccc5792980cfeb2152462
Angela Stephens <angelastephens317@gmail.com>
2020-06-22 22:19:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,8 +19,10 @@ class Song
    self.artist =Artist.find_or_create_by_name(name)
   end
   
-  def Song.new_by_filename
-  
+  def  def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
   end
   
 end
