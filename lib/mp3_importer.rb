b572f7f0f64784a93d66d76a72d518cad0e77261
Angela Stephens <angelastephens317@gmail.com>
2020-06-22 22:21:25 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,9 +1,11 @@
 class MP3Importer
   attr_accessor :path
   
-  def Song.new_by_filename(some_filename)
-    
+  def initialize(path)
+    @path=path
   end
+  
+  
   
   def files
     @files=files
