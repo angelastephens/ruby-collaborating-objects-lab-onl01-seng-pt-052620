f155330ad1186359d990edd45f41ca6f4aa03a90
Angela Stephens <angelastephens317@gmail.com>
2020-06-22 21:44:00 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+  attr_accessor :path
+  
+  def Song.new_by_filename(some_filename)
+    
+  end
+  
+  def files
+    @files=files
+    
+  end
+  
+  def import
+    
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  
+  attr_accessor :name, :artist
+  
+  @@all =[]
+  
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  
+  def self.all
+    @@all 
+  end
+  
+ 
+  
+  def artist_name=(name)
+   self.artist =Artist.find_or_create_by_name(name)
+  end
+  
+  def Song.new_by_filename
+    
+  
+end
